4ccc2e782fd2e62536c97f7310a40429a19bf47f
Raiwin.S <raiwin7@gmail.com>
2018-10-19 15:20:23 +0200

    enum fluids

---
+++ src/Gas_och_fluids_och_materials/Gas.java
@@ -0,0 +1,18 @@
+package Gas_och_fluids_och_materials;
+
+public enum Gas {
+	
+	AIR(1.29, 1.01), 
+	O2(1.43, 0.92), 
+	OXYGEN(1.43, 0.92)
+	;
+
+	double density;
+	double heatCapacity;
+
+	GasTable(double d, double hc) {
+		density = d;
+		heatCapacity = hc * 1E3; // 1E3 = 1*10^3
+		
+	}
+}
